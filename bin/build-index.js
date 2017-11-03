06746ed52ad5e3aa0c2974c878ea5064f99ffb28
Youri Tolstoy <ytolstoy@gmail.com>
2017-11-03 14:13:52 +0100

    bump and build a strong index for exact match

--- bin/build-index.js
+++ bin/build-index.js
@@ -1,28 +1,40 @@
 #!/usr/bin/env node
 
-const tokenizer = require('text-tokenize');
-var DATA_DIR = process.cwd() + '/data';
-var WEIGHTED_PROPS = ['code', 'name', 'description'];
+const tokenizer = require('text-tokenize'),
+    fs = require('fs'),
+    builder = require('./builder'),
+    DATA_DIR = process.cwd() + '/data',
+    data = require(DATA_DIR + '/data'),
+    WEIGHTED_PROPS = ['code', 'name', 'description'];
 
-var fs = require('fs');
-var builder = require('./builder')
-var data = require(DATA_DIR + '/data');
+console.log('Building strong index');
 
-console.log('Building index');
+const strongIndex = builder.build({
+    collection: data,
+    properties: ['name'],
+    tokenizer: function (value) {
+        return [value.toLowerCase().replace(/ /g, '')];
+    }
+});
 
-var index = builder.build({
+console.log('Writing file');
+
+fs.writeFile(DATA_DIR + '/data-index-strong.json', JSON.stringify(strongIndex), function (err) {
+    if (err) throw err;
+    console.log('Strong index successfully written to file!');
+});
+
+console.log('Building weaker index');
+
+const index = builder.build({
     collection: data,
     properties: WEIGHTED_PROPS,
-    tokenizer: function (value, property) {
-        const tokenized = tokenizer(value);
-        if (property === 'name') {
-            return [value.toLowerCase()].concat(tokenized);
-        }
-        return tokenized;
+    tokenizer: function (value) {
+        return tokenizer(value);
     }
-})
+});
 
-console.log('Writing file')
+console.log('Writing file');
 
 fs.writeFile(DATA_DIR + '/data-index.json', JSON.stringify(index), function (err) {
     if (err) throw err;
